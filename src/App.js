28bdfb994dea993b70548118b30271420da35928
Jingcheng Wang <jingwang49@gmail.com>
2024-01-23 23:49:23 +1100

    dog pic done

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import './App.css';
 import ColorRenderer from './01-color-renderer/ColorRenderer'
 import DarkMode from './02-dark-mode/DarkMode'
 import FormValidator from './03-form-validator/FormValidator'
+import DogPic from './04-dog-pic/DogPic'
 
 function App() {
   return (
     <div className="App">
       {/*<ColorRenderer />
-      <DarkMode />*/}
-      <FormValidator />
+      <DarkMode />
+      <FormValidator />*/}
+      <DogPic />
     </div>
   );
 }
