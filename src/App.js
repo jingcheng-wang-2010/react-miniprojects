a212ad23013de9e25db7cd23add3832596d6786d
Jingcheng Wang <jingwang49@gmail.com>
2024-01-24 19:56:30 +1100

    score keeper using local storage

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import ColorRenderer from './01-color-renderer/ColorRenderer'
 import DarkMode from './02-dark-mode/DarkMode'
 import FormValidator from './03-form-validator/FormValidator'
 import DogPic from './04-dog-pic/DogPic'
+import ScoreKeeper from './05-score-keeper/ScoreKeeper'
 
 function App() {
   return (
     <div className="App">
       {/*<ColorRenderer />
       <DarkMode />
-      <FormValidator />*/}
-      <DogPic />
+      <FormValidator />
+      <DogPic />*/}
+      <ScoreKeeper />
     </div>
   );
 }
